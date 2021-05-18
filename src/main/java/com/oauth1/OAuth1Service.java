66bf3f7e7833d1402704e7e8d9cf08c022c47dda
Anton De Silva <gardhiwasam@yahoo.com>
2021-05-17 21:25:17 -0400

    working signature

--- src/main/java/com/oauth1/OAuth1Service.java
+++ src/main/java/com/oauth1/OAuth1Service.java
@@ -10,54 +10,55 @@ import java.net.URISyntaxException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.time.Instant;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class OAuth1Service {
 
     public String buildSignature(HttpRequest request, String consumerKey, String consumerSecret, String token, String tokenSecret) {
-        String baseString =  generateBaseString(request, consumerKey, token);
-        System.out.print(baseString);
+        String baseString = generateBaseString(request, consumerKey, token);
         String secret = percentEncode(consumerSecret) + "&" + percentEncode(tokenSecret);
-        return new String(new HmacUtils(HmacAlgorithms.HMAC_SHA_1, secret.getBytes()).hmac(baseString.getBytes()), StandardCharsets.UTF_8);
+        byte[] signature = new HmacUtils(HmacAlgorithms.HMAC_SHA_1, secret.getBytes()).hmac(baseString.getBytes());
+        return Base64.getEncoder().encodeToString(signature);
     }
 
     private String generateBaseString(HttpRequest request, String consumerKey, String token) {
-        String paramString = URLEncoder.encode(generateParameterString(request, consumerKey, token)).replace("+", "%20");
         String method = request.getMethod().name();
-        // this should be just base url
-        String url = URLEncoder.encode(request.getUrl(), StandardCharsets.UTF_8).replace("+", "%20");
-
-        return method + "&" + url + "&" + paramString;
+        String url = percentEncode(getUrlWithoutParameters(request.getUrl()));
+        String paramString = percentEncode(generateParameterString(request, consumerKey, token));
+        return method.concat("&").concat(url).concat("&").concat(paramString);
     }
 
     private String generateParameterString(HttpRequest request, String consumerKey, String token) {
+        // get the standard set of params that will always be there
         List<Param> params = getStandardOAuthParams(consumerKey, token);
 
         // extract url params
         List<Param> queryParams = getQueryParams(request);
         params.addAll(queryParams);
+
         // extract any form URL encode values from body
         List<Param> bodyParams = getBodyParams(request);
         params.addAll(bodyParams);
 
         // first, url encode each key-value pair
-        params = params.stream().map(param -> new Param(URLEncoder.encode(param.getName(), StandardCharsets.UTF_8).replace("+", "%20"),
-                URLEncoder.encode(String.valueOf(param.getValue()), StandardCharsets.UTF_8).replace("+", "%20")))
+        params = params.stream()
+                .map(param -> new Param(percentEncode(param.getName()), percentEncode(param.getValue())))
                 .collect(Collectors.toList());
         // sort the params
         params = params.stream().sorted().collect(Collectors.toList());
 
-        return params.stream().map(param -> param.getName() + "=" + param.getValue()).collect(Collectors.joining("&"));
+        return params.stream()
+                .map(param -> param.getName() + "=" + param.getValue())
+                .collect(Collectors.joining("&"));
     }
 
     private List<Param> getStandardOAuthParams(String consumerKey, String token) {
         List<Param> params = new ArrayList<>();
         params.add(new Param("oauth_consumer_key", consumerKey));
-        params.add(new Param("oauth_nonce", UUID.randomUUID().toString()));
+        params.add(new Param("oauth_nonce", "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg"/*UUID.randomUUID().toString())*/));
         params.add(new Param("oauth_signature_method", "HMAC-SHA1"));
-        params.add(new Param("oauth_timestamp", String.valueOf(Instant.now().getEpochSecond())));
+        params.add(new Param("oauth_timestamp", "1318622958" /*String.valueOf(Instant.now().getEpochSecond()))*/));
         params.add(new Param("oauth_version", "1.0"));
 
         if (token != null) {
@@ -68,10 +69,11 @@ public class OAuth1Service {
     }
 
     private List<Param> getQueryParams(HttpRequest request) {
-        List<Param> params = new ArrayList<>();
+        final List<Param> params = new ArrayList<>();
+
+        // extract any query params that may be in the URL already
         try {
-            List<NameValuePair> urlParams = URLEncodedUtils
-                    .parse(new URI(request.getUrl()), StandardCharsets.UTF_8);
+            List<NameValuePair> urlParams = URLEncodedUtils.parse(new URI(request.getUrl()), StandardCharsets.UTF_8);
             for (NameValuePair pair : urlParams) {
                 params.add(new Param(pair.getName(), pair.getValue()));
             }
@@ -79,7 +81,7 @@ public class OAuth1Service {
             throw new IllegalArgumentException("Invalid url " + e.getMessage(), e);
         }
 
-        // also add any query params
+        // also add any query params that are supplied separately
         if (request.getQueryParams() != null) {
             for (Map.Entry<String, List<String>> entry : request.getQueryParams().entrySet()) {
                 List<String> values = entry.getValue();
@@ -92,19 +94,40 @@ public class OAuth1Service {
     }
 
     private List<Param> getBodyParams(HttpRequest request) {
-        List<Param> params = new ArrayList<>();
-        final String body = request.getBody();
-        String[] urlEncodedParams = body.split("&");
-        for (String encodedPair : urlEncodedParams) {
-            String[] pair = encodedPair.split("=");
-            String decodedName = URLDecoder.decode(pair[0], StandardCharsets.UTF_8);
-            String decodedValue = URLDecoder.decode(pair[1], StandardCharsets.UTF_8);
-            params.add(new Param(decodedName, decodedValue));
+        final List<Param> params = new ArrayList<>();
+        final Map<String, List<String>> headers =request.getHeaders();
+        if (headers != null) {
+            final List<String> contentType = headers.get("Content-Type");
+            if (contentType != null && contentType.contains("application/x-www-form-urlencoded")) {
+                final String body = request.getBody();
+                String[] urlEncodedParams = body.split("&");
+                for (String encodedPair : urlEncodedParams) {
+                    String[] pair = encodedPair.split("=");
+                    String decodedName = URLDecoder.decode(pair[0], StandardCharsets.UTF_8);
+                    String decodedValue = URLDecoder.decode(pair[1], StandardCharsets.UTF_8);
+                    params.add(new Param(decodedName, decodedValue));
+                }
+            }
         }
         return params;
     }
 
     private String percentEncode(String toEncode) {
-        return URLEncoder.encode(toEncode, StandardCharsets.UTF_8).replace("+", "%20");
+        return URLEncoder.encode(toEncode, StandardCharsets.UTF_8)
+                .replace("+", "%20");
     }
+
+    private String getUrlWithoutParameters(String url) {
+        try {
+            URI uri = new URI(url);
+            return new URI(uri.getScheme(),
+                    uri.getAuthority(),
+                    uri.getPath(),
+                    null, // Ignore the query part of the input url
+                    uri.getFragment()).toString();
+        } catch (URISyntaxException u) {
+            throw new IllegalArgumentException("Invalid URL!");
+        }
+    }
+
 }
--- src/main/java/com/oauth1/Runner.java
+++ src/main/java/com/oauth1/Runner.java
@@ -1,15 +1,73 @@
 package com.oauth1;
 
+import com.github.scribejava.apis.TwitterApi;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.*;
+import com.github.scribejava.core.oauth.OAuth10aService;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+
 public class Runner {
     public static void main(String... args) {
+
+        final OAuth10aService service = new ServiceBuilder("your client id")
+                .apiSecret("your client secret")
+                .build(TwitterApi.instance());
 
         OAuth1Service oAuth1Service = new OAuth1Service();
 
         HttpRequest request = new HttpRequest();
+        request.setHeaders(Map.of("Content-Type", List.of("application/x-www-form-urlencoded")));
         request.setUrl("https://api.twitter.com/1.1/statuses/update.json?include_entities=true");
         request.setMethod(HttpRequest.Method.POST);
         request.setBody("status=Hello%20Ladies%20%2b%20Gentlemen%2c%20a%20signed%20OAuth%20request%21");
-        String s = oAuth1Service.buildSignature(request, "xvz1evFS4wEEPTGEFPHBog", "", "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", "");
+        String s = oAuth1Service.buildSignature(request, "xvz1evFS4wEEPTGEFPHBog", "kAcSOqF21Fu85e7zjz7ZN2U4ZRhfV3WpwPAoE3Z7kBw", "370773112-GmHxMAgYyLbNEtIKZeRNFsMKPR9EyMZeS9weJAEb", "LswwdoUaIvS8ltyTt5jkRh4J50vUPVVHtR2YPi5kE");
         System.out.println(s);
+    }
+
+    public static void djk() throws IOException, ExecutionException, InterruptedException {
+        final OAuth10aService service = new ServiceBuilder("your client id")
+                .apiSecret("your client secret")
+                .build(TwitterApi.instance());
+        final Scanner in = new Scanner(System.in);
+
+        System.out.println("=== Twitter's OAuth Workflow ===");
+        System.out.println();
+
+        // Obtain the Request Token
+        System.out.println("Fetching the Request Token...");
+        final OAuth1RequestToken requestToken = service.getRequestToken();
+        System.out.println("Got the Request Token!");
+        System.out.println();
+
+        System.out.println("Now go and authorize ScribeJava here:");
+        System.out.println(service.getAuthorizationUrl(requestToken));
+        System.out.println("And paste the verifier here");
+        System.out.print(">>");
+        final String oauthVerifier = in.nextLine();
+        System.out.println();
+
+        // Trade the Request Token and Verifier for the Access Token
+        System.out.println("Trading the Request Token for an Access Token...");
+        final OAuth1AccessToken accessToken = service.getAccessToken(requestToken, oauthVerifier);
+        System.out.println("Got the Access Token!");
+        System.out.println("(The raw response looks like this: " + accessToken.getRawResponse() + "')");
+        System.out.println();
+
+        // Now let's go and ask for a protected resource!
+        System.out.println("Now we're going to access a protected resource...");
+        final OAuthRequest request = new OAuthRequest(Verb.GET, "");
+        service.signRequest(accessToken, request);
+        try (Response response = service.execute(request)) {
+            System.out.println("Got it! Lets see what we found...");
+            System.out.println();
+            System.out.println(response.getBody());
+        }
+        System.out.println();
+        System.out.println("That's it man! Go and build something awesome with ScribeJava! :)");
     }
 }
